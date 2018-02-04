da052bdb15d1e69a8cfa89fc032c4b80048492f3
MKagesawa <mk5986@nyu.edu>
2018-02-04 14:04:27 +0800

    added Stochastic Gradient Descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 from sympy import *
 import pickle
+import random
 
 norm_list_obj = open("norm_list.pkl", "rb")
 norm_list = pickle.load(norm_list_obj)
 
 
-def train_model(normalized, num_iter):
+def train_model(normalized, max_iter):
 
     # Initializing w0, w1, w2 parameters
     weights = [0.0, 0.0, 0.0]
 
     # Learning Rates
-    alpha = [0.15, 0.01, 0.1, 0.3]
+    alpha = [0.15, 0.01, 0.1, 0.3, 0.05, 0.5]
 
     x1 = [normalized[i][0] for i in range(len(normalized))]
     x2 = [normalized[i][1] for i in range(len(normalized))]
@@ -24,16 +25,16 @@ def train_model(normalized, num_iter):
     loss_function = sum([(weights[0] + weights[1] * x1[i] + weights[2] * x2[i] - y[i]) ** 2 for i in range(m)])
     loss_function = loss_function / (2*m)
 
-    for i in range(num_iter):
+    for i in range(max_iter):
 
         gradient0 = (sum([(weights[0] + weights[1] * x1[a] + weights[2] * x2[a] - y[a]) for a in range(m)])) * 1/m
         gradient1 = (sum([(weights[0] + weights[1] * x1[b] + weights[2] * x2[b] - y[b]) * x1[b] for b in range(m)])) * 1/m
         gradient2 = (sum([(weights[0] + weights[1] * x1[c] + weights[2] * x2[c] - y[c]) * x2[c] for c in range(m)])) * 1/m
 
         # Update weights
-        weights[0] = weights[0] - (alpha[0] * gradient0)
-        weights[1] = weights[1] - (alpha[0] * gradient1)
-        weights[2] = weights[2] - (alpha[0] * gradient2)
+        weights[0] = weights[0] - (alpha[2] * gradient0)
+        weights[1] = weights[1] - (alpha[2] * gradient1)
+        weights[2] = weights[2] - (alpha[2] * gradient2)
 
         loss_function = sum([(weights[0] + weights[1] * x1[i] + weights[2] * x2[i] - y[i]) ** 2 for i in range(m)])
         loss_function = loss_function * 1/(2*m)
@@ -41,4 +42,47 @@ def train_model(normalized, num_iter):
     return weights, loss_function
 
 
-print(train_model(norm_list, 1000))
+# Stochastic Gradient Descent
+def SGD(weights):
+
+    # Select a batch of data to train
+    start = random.randint(0, len(norm_list) - 4)
+    data_len = random.randint(4, len(norm_list) - start)
+
+    data_batch = norm_list[start:start+data_len]
+    print(start)
+    print(data_len)
+    print(data_batch)
+
+    loss_function = 0
+
+    # Each update of w uses only a single data point X^(i)
+    for d in data_batch:
+        g0 = weights[0] - 0.1 * (weights[0] + weights[1] * d[0] + weights[2] * d[1] - d[2])  # alpha set to 0.1
+        g1 = weights[1] - 0.1 * (weights[0] + weights[1] * d[0] + weights[2] * d[1] - d[2]) * d[0]
+        g2 = weights[2] - 0.1 * (weights[0] + weights[1] * d[0] + weights[2] * d[1] - d[2]) * d[1]
+
+        # Update weights
+        weights[0] = g0
+        weights[1] = g1
+        weights[2] = g2
+
+        loss_function = sum([(weights[0] + weights[1] * norm_list[i][0] + weights[2] * norm_list[i][1] - norm_list[i][2]) ** 2 for i in range(len(norm_list))])
+        loss_function = loss_function * 1/(2*data_len)
+    print(loss_function)
+    return weights
+
+print (SGD(SGD(SGD([0.0, 0.0, 0.0]))))
+
+# Plot J(w) for with different alpha and number of iterations
+def plot():
+    J = []
+    iterations = [10, 20, 30, 40, 50, 60, 70, 80]
+    for i in iterations:
+        J.append(train_model(norm_list, i)[1])
+    plt.plot(iterations, J, 'ro')
+
+    plt.show()
+
+#Testing
+#print(train_model(norm_list, 8000))
